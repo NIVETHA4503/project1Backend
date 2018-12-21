f4309b403351ff383a4b4222ec15ba52e0fc6be2
Welcome <Welcome@LAPTOP-44NOGFTG>
2018-12-21 23:55:37 +0530

    dec-21 alteration upto dec-19 (completion of Browse all products:info,delete,edit & Add products all these done directly by using frontend). Before validation.

--- project1backend/src/main/java/com/niit/dao/ProductDao.java
+++ project1backend/src/main/java/com/niit/dao/ProductDao.java
@@ -1,12 +1,15 @@
 package com.niit.dao;
 
+import java.util.List;
+
 import com.niit.models.Product;
 
 public interface ProductDao {
-	void addProduct(Product product);
+//	void addProduct(Product product);
 	Product getProduct(int id);
 	void deleteProduct(int id);
-	void updateProduct(Product product);
-	
+//	void updateProduct(Product product);
+	List<Product> getAllProducts();
+	void saveOrUpdate(Product product);
 
 }
--- project1backend/src/main/java/com/niit/dao/ProductDaoImpl.java
+++ project1backend/src/main/java/com/niit/dao/ProductDaoImpl.java
@@ -1,14 +1,13 @@
 package com.niit.dao;
 
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.niit.models.Product;
-
-import antlr.collections.List;
 @Repository  
 @Transactional 
 public class ProductDaoImpl implements ProductDao {
@@ -17,10 +16,10 @@ private SessionFactory sessionFactory;
 	public ProductDaoImpl(){
 		System.out.println("ProductDaoImpl bean is created..");
 	}
-	public void addProduct(Product product) {
-       Session session= sessionFactory.getCurrentSession();
-       session.save(product);
-	}
+//	public void addProduct(Product product) {
+//       Session session= sessionFactory.getCurrentSession();
+//       session.save(product);
+//	}
 	public Product getProduct(int id) {
 		Session session=sessionFactory.getCurrentSession();
 		Product product=(Product)session.get(Product.class, id);
@@ -31,9 +30,20 @@ private SessionFactory sessionFactory;
 		Product product=(Product)session.get(Product.class, id);
 		session.delete(product);
 	}
-	public void updateProduct(Product product) {
+//	public void updateProduct(Product product) {
+//		Session session=sessionFactory.getCurrentSession();
+//		session.update(product);
+//	}
+	public java.util.List<Product> getAllProducts() {
 		Session session=sessionFactory.getCurrentSession();
-		session.update(product);
+		Query query=session.createQuery("from Product");
+		List<Product> products =query.list();
+		return products;
+	}
+	public void saveOrUpdate(Product product) {
+	Session session=sessionFactory.getCurrentSession();
+	session.saveOrUpdate(product);
+		
 	}
 	
 	
