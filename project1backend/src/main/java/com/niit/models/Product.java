e06d6562b285c1122e20ea72c254e34140bdceb3
Welcome <Welcome@LAPTOP-44NOGFTG>
2018-12-26 10:07:51 +0530

    before RDBMS

--- project1backend/src/main/java/com/niit/models/Product.java
+++ project1backend/src/main/java/com/niit/models/Product.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 
@@ -13,10 +16,15 @@ public class Product {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	@Column(name="pname")
+	@NotEmpty(message="Product Name cannot be empty")
 private String productname;
+	@NotEmpty(message="Description is mandatory")
 private String description;
+@Min(value=0,message="Quantity cannot be less than 0")
 private int quantity;
+@Min(value=1,message="Price cannot be less than 1")
 private double price;
+
 public int getId() {
 	return id;
 }
